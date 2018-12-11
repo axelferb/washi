87b315346c7b25639f9a5e187791008fc7f204b5
Axel <axel.ferb@gmail.com>
2018-12-11 17:36:12 +0100

    Testing an idea with props on the boxes

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,26 @@
     <navbar/>
     <main class="Page-main">
       <div class="Page-container">
-        <router-view></router-view>
+        <!-- <router-view></router-view> -->
       </div>
+      <props/>
     </main>
     <w-footer/>
   </div>
 </template>
 
 <script>
+// console.log(props.data)
 import Navbar from './components/Navbar.vue';
 import WFooter from './components/WFooter.vue';
+import Props from './props/props.vue';
+console.log(Props.props)
 export default {
   name: 'app',
   components: {
     Navbar,
-    WFooter
+    WFooter,
+    Props
   }
 }
 </script>
---
+++ src/props/props.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div v-for="n in 2" :key="n" style="height:100px; width:100px; background-color:grey;">
+      <h1>
+        Hello!
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    booked: Boolean,
+    appNo: Number
+  },
+  data () {
+    return {
+      bananas: {
+        color: 'Yellow'
+      }
+    }
+  }
+}
+</script>
