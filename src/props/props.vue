e5156aa7885bd1ebee8ebf5621a3aed92bb58eda
axelferb <axel.ferb@gmail.com>
2018-12-17 11:42:12 +0100

    Props seems to work ok-ish, will need to make appNo work aswell, now only booked works

--- src/props/props.vue
+++ src/props/props.vue
@@ -1,9 +1,12 @@
 <template>
   <div style="display:flex; justify-content:center; align-items:center; margin:15px; auto;">
-    <div class="Loopers" v-for="n in 5" :key="n" style="display:flex; justify-content:center; align-items:center; height:100px; width:100px; background-color:grey; box-sizing:border-box; border:1px solid white;">
-      <p>
-        {{this.$props}}
-      </p>
+    <div
+      class="Loopers"
+      v-for="n in 5"
+      :key="n"
+      style="display:flex; justify-content:center; align-items:center; height:100px; width:100px; background-color:grey; box-sizing:border-box; border:1px solid white;"
+    >
+      <p>{{this.$props}}</p>
     </div>
   </div>
 </template>
@@ -11,21 +14,20 @@
 <script>
 export default {
   props: {
-    booked: 'Hello!',
-    appNo: String
+    booked: Boolean,
+    appNo: Number
   },
-  data () {
-    return {
-
-      }
+  data() {
+    return {};
   },
   methods: {
-    book: function (event) {
-      }
+    book: function(event) {}
   },
   created: function() {
-    console.log(this.$props + 'Hello!')
-    console.log(this.$props.booked)
+    console.log("Kuken");
+    console.log(this);
+    console.log(this.$props);
+    console.log(this.$props.booked);
   }
-}
+};
 </script>
